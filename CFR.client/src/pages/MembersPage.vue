6d7a70f849cc553c7ee4be4e3869dd41fdc1ca86
Jaden Hawkes <jadenhawkes@gmail.com>
2023-05-01 22:26:00 -0600

    Minor content, some more things stubbed out

---
+++ CFR.client/src/pages/MembersPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="component">
+    <!-- This will contain profiles of all current officer and like staff -->
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
