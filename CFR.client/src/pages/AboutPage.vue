5cadf93e096e5c89de9f8294112045c06a98b4a1
Jaden Hawkes <jadenhawkes@gmail.com>
2023-05-01 21:57:57 -0600

    Minor content dump

--- CFR.client/src/pages/AboutPage.vue
+++ CFR.client/src/pages/AboutPage.vue
@@ -1,6 +1,42 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <p>On the medical side, we have 3 goals:
+          o Professional medical services at the front
+          o Establishing a backline-to-frontline logistics system that allows us to be self-sufficient and not depend on
+          public logistics
+          o Providing medical support to other regiments' operations on demand</p>
+      </div>
+      <div class="col-md-6">
+        <p> We also take interest in:
+          o Helping new players grasp the game mechanics
+          o Building defenses and possibly maintaining a base
+          o Logistics, not limited to medical needs
+          o Various operations that allow us to experience non-medical aspects of the game
+
+          We warmly welcome you into our community!</p>
+      </div>
+    </div>
+    <section>
+      <h1>Colonial First Responders</h1>
+      <h2><b>In Blood We Trust!</b></h2>
+
+      <p>Founded on the 1st of January 2023 as a natural successor to the Colonial Medical Battalion, our regiment retains
+        its focus on medical and support roles, while attempting to build a friendly and engaging community with the help
+        of
+        its experienced members and commanders.
+
+        We consider ourselves to be a casual regiment - no playtime quotas, no strict command structure, no enforced
+        medical
+        duties. While all members are expected to attend medical duties to an extent, we mix it up with infantry,
+        building,
+        tanks and artillery from time to time, although these do not occur on a regular basis.
+      </p>
+
+
+
+    </section>
   </div>
 </template>
 
