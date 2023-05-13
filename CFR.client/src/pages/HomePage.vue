60ea515940c05177021ff6dc50ad643f3be7593a
HawkesJ <jadenhawkes@gmail.com>
2023-05-13 10:35:20 -0600

    More messings

--- CFR.client/src/pages/HomePage.vue
+++ CFR.client/src/pages/HomePage.vue
@@ -10,47 +10,31 @@
         <h3><i>In Blood We Trust.</i></h3>
       </div>
     </div>
+    <div id="slideshow">
+      <div class="slide-wrapper">
+
+        <!-- Define each of the slides
+         and write the content -->
+
+        <div class="slide">
+          <img
+            src="https://media.discordapp.net/attachments/1097757077029539850/1106970069151535235/MedicSwarm.png?width=911&height=512"
+            class="slideimg" alt="">
+        </div>
+        <div class="slide">
+          <img
+            src="https://media.discordapp.net/attachments/1059340116168015882/1094828091265781820/image.png?width=706&height=513"
+            class="slideimg" alt="">
+        </div>
+        <div class="slide">
+          <img src="https://cdn.discordapp.com/attachments/1059340116168015882/1105446026309029948/20230508182042_1.jpg"
+            class="slideimg" alt="">
+        </div>
+        <div class="slide">
+        </div>
+      </div>
+    </div>
   </div>
-
-
-
-
-  <!-- <div class="container-fluid mt-5">
-    <div class="row">
-      <div class="col-12 text-center">
-        <img src="https://cdn.discordapp.com/attachments/1097757077029539850/1102856277501165578/CFR_Simple_Logo.png"
-          alt="CFR logo" class="CFRLogoMain">
-      </div>
-    </div>
-    <div class="row text-center justify-content-center">
-      <div class="col-2">
-        <RouterLink :to="{ name: 'About' }" class="">
-          <div class="button btn btn-primary">About</div>
-        </RouterLink>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-2">
-        <RouterLink :to="{ name: 'Members' }">
-          <div class="button btn btn-primary">Members</div>
-        </RouterLink>
-      </div>
-
-      <div>WUH</div>
-      <div class="col-2">
-        <RouterLink :to="{ name: 'Gallery' }">
-          <div class="button btn btn-primary">Gallery</div>
-        </RouterLink>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-2 text-center">
-        <RouterLink :to="{ name: 'Account' }" class="btn btn-primary">
-          <div class="button">Account</div>
-        </RouterLink>
-      </div>
-    </div>
-  </div> -->
 </template>
 <!-- TODO re-write the homepage to merge about into it  -->
 
@@ -105,5 +89,67 @@ export default {
   background-repeat: no-repeat;
   background-size: cover;
   background-attachment: fixed;
+}
+
+body {
+  font-family: Helvetica, sans-serif;
+  padding: 5%;
+  text-align: center;
+  font-size: 50;
+}
+
+/* Styling the area of the slides */
+#slideshow {
+  overflow: hidden;
+  height: 1080px;
+  width: 1080px;
+  margin: 0 auto;
+}
+
+/* Style each of the sides
+with a fixed width and height */
+.slide {
+  float: left;
+  height: 1080px;
+  width: 1080px;
+}
+
+.slideimg {
+  max-height: 1080px;
+}
+
+/* Add animation to the slides */
+.slide-wrapper {
+
+  /* Calculate the total width on the
+basis of number of slides */
+  width: calc(1080px * 4);
+
+  /* Specify the animation with the
+duration and speed */
+  animation: slide 15s ease infinite;
+}
+
+/* Define the animation
+for the slideshow */
+@keyframes slide {
+
+  /* Calculate the margin-left for
+each of the slides */
+  20% {
+    margin-left: 0px;
+  }
+
+  40% {
+    margin-left: calc(-1080px * 1);
+  }
+
+  60% {
+    margin-left: calc(-1080px * 2);
+  }
+
+  80% {
+    margin-left: calc(-1080px * 3);
+  }
 }
 </style>
